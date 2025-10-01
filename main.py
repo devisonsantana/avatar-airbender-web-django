02a28973f3de7e9f0410de5b4b916ae830cad7a8
devisonsantana <devisonsantana@hotmail.com>
2025-10-01 20:57:43 -0300

    feat: 'traduzir' func implemented
    Implements 'traduzir' function and prints the response

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import requests
+from tradutor import traduzir
 
 url:str = "https://last-airbender-api.fly.dev/api/v1/characters/" # Total 500 Characters ?perPage= &page=
 query:str = f"perPage=10&page=5"
 
 response:list[dict] = requests.get(url=url, params=query).json()
+
+personagens = traduzir(response)
+
+divisoria = "-" *30
+
+for p in personagens:
+    print(divisoria)
+    print(f'nome = {p.name}')
+    print(f'afiliacao = {p.affiliation}')
