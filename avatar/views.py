7ecec06bcfc19d92ce730ca1c273f35106b8a0d0
devisonsantana <devisonsantana@hotmail.com>
2025-10-04 16:20:52 -0300

    feat: add bootstrap styles

--- avatar/views.py
+++ avatar/views.py
@@ -7,12 +7,12 @@ from googletrans import Translator
 # Create your views here.
 def characters(request):
     url:str = "https://last-airbender-api.fly.dev/api/v1/characters/" # Total 500 Characters ?perPage= &page=
-    query:str = f"perPage=5&page=1"
+    query:str = f"perPage=10&page=1"
 
     response:list[dict] = requests.get(url=url, params=query).json()
     for character in response:
-        afiliacao = character.get("affiliation", "none")
-        character["afiliação_traduzida"] = asyncio.run(Translator().translate(afiliacao, 'pt', 'en')).text
-        nome = character.get("name", "none")
+        afiliacao = character.get("affiliation")
+        character["afiliacao_traduzida"] = asyncio.run(Translator().translate(afiliacao, 'pt', 'en')).text
+        nome = character.get("name")
         character["nome_traduzido"] = asyncio.run(Translator().translate(nome, 'pt', 'en')).text
-    return render(request, 'index.html', {'response':response})
+    return render(request, 'index.html', {'personagem':response})
