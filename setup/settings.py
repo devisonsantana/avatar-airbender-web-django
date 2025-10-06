0e5d8abb0eba14e1d48c97a1a287468d601c234b
devisonsantana <devisonsantana@hotmail.com>
2025-10-06 02:13:39 -0300

    feat(deploy): add deploy configuration

--- setup/settings.py
+++ setup/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-1w%dgg&kvi+#pj+s(r0^3h5em%io5n%utpy9szme)4n=^zzl_='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
