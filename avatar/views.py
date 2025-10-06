cc0a999b2dee0ec45b099c5ecfffe4e3017e6969
devisonsantana <devisonsantana@hotmail.com>
2025-10-06 01:12:09 -0300

    feat: add next page, previous page and page limit

--- avatar/views.py
+++ avatar/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-import requests
-import asyncio
+from django.http import HttpRequest
+import requests, asyncio
 from googletrans import Translator
 
 # Create your views here.
-def characters(request):
+def characters(request:HttpRequest):
     url:str = "https://last-airbender-api.fly.dev/api/v1/characters/" # Total 500 Characters ?perPage= &page=
-    query:str = f"perPage=10&page=1"
+    page:int = request.GET.get('page', 1)
+    query:str = f"perPage=10&page={page}"
 
     response:list[dict] = requests.get(url=url, params=query).json()
     for character in response:
@@ -15,4 +15,4 @@ def characters(request):
         character["afiliacao_traduzida"] = asyncio.run(Translator().translate(afiliacao, 'pt', 'en')).text
         nome = character.get("name")
         character["nome_traduzido"] = asyncio.run(Translator().translate(nome, 'pt', 'en')).text
-    return render(request, 'index.html', {'personagem':response})
+    return render(request, 'index.html', {'personagem': response, 'page': int(page)})
