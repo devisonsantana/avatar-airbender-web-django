21206ff0b7a9e01dad864fd3f6e083527f049d39
devisonsantana <devisonsantana@hotmail.com>
2025-10-30 00:49:16 -0300

    chore(deploy): configure deployment
    - this fixes the staticfiles issue
    
    closes #2

--- setup/settings.py
+++ setup/settings.py
@@ -120,15 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+# Define the directories where static files will be collected from
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'avatar/static')]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    BASE_DIR / "avatar/static"
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
